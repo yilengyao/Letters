7cf4522cec7d22a90f9396bc5b20b16d28546702
Yi leng Yao <yilengyao@gmail.com>
2024-09-19 19:02:38 -0700

    component lifecycle

--- src/index.js
+++ src/index.js
@@ -4,189 +4,141 @@ import PropTypes from 'prop-types';
 
 const node = document.getElementById('root');
 
-const data = {
-  post: {
-    id: 123,
-    content:
-      "What we hope ever to do with ease, we must first learn to do with diligence. — Samuel Johnson",
-    user: "Mark Thomas"
-  },
-  comments: [
-    {
-      id: 0,
-      user: "David",
-      content: "such. win."
-    },
-    {
-      id: 1,
-      user: "Haley",
-      content: "Love it."
-    },
-    {
-      id: 2,
-      user: "Peter",
-      content: "Who was Samuel Johnson?"
-    },
-    {
-      id: 3,
-      user: "Mitchell",
-      content: "@Peter get off Letters and do your homework"
-    },
-    {
-      id: 4,
-      user: "Peter",
-      content: "@mitchell ok :P"
+class ChildComponent extends Component {
+  
+  static propTypes = {
+    name: PropTypes.string
+  };
+
+  static defaultProps = (function() {
+    console.log("ChildComponent : defaultProps");
+    return {};
+  })();
+
+  constructor(props) {
+    super(props);
+    console.log("ChildComponent : state ");
+    this.state = {
+      name: "Mark"
+    };
+    this.oops = this.oops.bind(this);
+  }
+
+  componentWillMount() {
+    console.log("ChildComponent : componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("ChildComponent : componentDidMount");
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("ChildComponent : componentWillReceiveProps()");
+    console.log("nextProps: ", nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("<ChildComponent/> - shouldComponentUpdate()");
+    console.log("nextProps: ", nextProps);
+    console.log("nextState: ", nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("<ChildComponent/> - componentWillUpdate");
+    console.log("nextProps: ", nextProps);
+    console.log("nextState: ", nextState);
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    console.log("ChildComponent: componentDidUpdate");
+    console.log("previousProps:", previousProps);
+    console.log("previousState:", previousState);
+  }
+
+  componentWillUnmount() {
+    console.log("ChildComponent: componentWillUnmount");
+  }
+
+  oops() {
+    this.setState(() => ({ opps: true }));
+  }
+
+  render() {
+    if (this.state.oops) {
+      throw new Error("Something went wrong");
     }
-  ]
-};
-
-class Post extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="post">
-        <h2 className="postAuthor">{this.props.user}</h2>
-        <span className="postBody">{this.props.content}</span>
-        {this.props.children}
-      </div>
-    );
+    console.log("ChildComponent: render");
+    return [
+      <div key="name">Name: {this.props.name}</div>,
+      <button key="error" onClick={this.oops}>
+        Create error
+      </button>
+    ];
   }
 }
 
-Post.propTypes = {
-  user: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
-};
-
-class Comment extends Component {
-  render() {
-    return (
-      <div className="Comment">
-        <h2 className="commentAuthor">{this.props.user + " : "}</h2>
-        <span className="commentContent">{this.props.content}</span>
-      </div>
-    );
-  }
-}
-
-Comment.propTypes = {
-  id: PropTypes.number.isRequired,
-  content: PropTypes.string.isRequired,
-  user: PropTypes.string.isRequired
-};
-
-class CreateComment extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: "",
-      user: ""
+class ParentComponent extends Component {
+  
+  static defaultProps = (function() {
+    console.log("ParentComponent: defaultProps");
+    return {
+      true: false
     };
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  })();
 
-  handleUserChange(event) {
-    const val = event.target.value;
-    this.setState(() => ({
-      user: val
-    }));
-  }
-
-  handleTextChange(event) {
-    const val = event.target.value;
-    this.setState({
-      content: val
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onCommentSubmit({
-      user: this.state.user.trim(),
-      content: this.state.content.trim()
-    });
-    this.setState(() => ({
-      user: "",
-      content: ""
-    }));
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="createComment">
-        <input
-          value={this.state.user}
-          onChange={this.handleUserChange}
-          placeholder="Your name"
-          type="text"
-        />
-        <input
-          value={this.state.content}
-          onChange={this.handleTextChange}
-          placeholder="Thoughts?"
-          type="text"
-        />
-        <button type="submit">Post</button>
-      </form>
-    )
-  }
-}
-
-CreateComment.propTypes = {
-  onCommentSubmit: PropTypes.func.isRequired,
-  content: PropTypes.string
-};
-
-class CommentBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      comments: this.props.comments
-    };
-    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    console.log("ParentComponent: state");
+    this.state = { text: "" };
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
-  handleCommentSubmit(comment) {
-    const comments = this.state.comments;
-    comment.id = Date.now();
-    const newComments = comments.concat([comment]);
-    this.setState({
-      comments: newComments
-    });
+  componentWillMount() {
+    console.log("ParentComponent: componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("ParentComponent: componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("ParentComponent: comnponentWillUnmount");
+  }
+
+  onInputChange(e) {
+    const text = e.target.value;
+    this.setState(() => ({ text: text }));
+  }
+
+  componentDidCatch(err, errorInfo) {
+    console.log("componentDidCatch");
+    console.error(err);
+    console.error(errorInfo);
+    this.setState(() => ({ err, errorInfo }));
   }
 
   render() {
-      return(
-        <div className="commentBox">
-          <Post
-            id={this.props.post.id}
-            content={this.props.post.content}
-            user={this.props.post.user}
-          />
-          {this.state.comments.map(function(comment) {
-            return (
-              <Comment
-                key={comment.id}
-                content={comment.content}
-                user={comment.user}
-              />
-            );
-          })}
-          <CreateComment onCommentSubmit={this.handleCommentSubmit} />
-        </div>
-      )
+    console.log("ParentComponent: render");
+    if (this.state.err) {
+      return (
+        <details style={{ whiteSpace: "pre-wrap" }}>
+          {this.state.error && this.state.error.toString()}
+          <br />
+          {this.state.errorInfo.componentStack}
+        </details>
+      );
+    }
+    return [
+      <h2 key="h2">Learn about rendering and lifecycle methods!</h2>,
+      <input
+        key="input"
+        value={this.state.text}
+        onChange={this.onInputChange}
+      />,
+      <ChildComponent key="ChildComponent" name={this.state.text} />
+    ];
   }
 }
 
-CommentBox.propTypes = {
-  post: PropTypes.object,
-  comments: PropTypes.arrayOf(PropTypes.object)
-};
-
-ReactDOM.createRoot(node).render(<CommentBox comments={data.comments} post={data.post} />);
+ReactDOM.createRoot(node).render(<ParentComponent />);
